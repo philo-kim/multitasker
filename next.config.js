48124b44da64d679a4e13bc4ca0ea05b367d6bb6
philo-kim <fineday9@gmail.com>
2025-07-30 18:10:00 +0900

    try to build

--- next.config.js
+++ next.config.js
@@ -2,17 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    optimizeCss: true,
-  },
-  // Vercel 배포 최적화
-  images: {
-    domains: [],
-  },
-  // 환경변수 확인
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
-  },
 }
 
 module.exports = nextConfig
